1d71830750d1e1e4ec84c723d5996c839bb6afc6
Gegene79 <fabienvignat@yahoo.fr>
2017-09-16 16:18:47 +0200

    primera version funcional - faltan tablas todavia

--- unload/scr/unload.sh
+++ unload/scr/unload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+
 #   Unload script to extract data from "Gorrion" system.
 #   
 #   The script generates a file unload_gorrion_YYYYMMDD.tar.gz that contains
@@ -18,9 +18,11 @@ set -x
 #           &2: stderr redirected to sdtout.
 #           
 
-# path y prefijo de los logs
-logs_path="../logs/"
-file_prefix="unload_gorrion_"
+# Importación de parametros del script
+if ! . ../conf/params.cfg; then
+    printf "Error: no se puede acceder al fichero de parametros.\n"
+    exit 1;
+fi
 
 # redirigimos stdout y stderr hacia el log.
 exec 1>>"$logs_path""$file_prefix"$(date +'%Y-%m-%d').log 2>&1
@@ -28,17 +30,11 @@ exec 1>>"$logs_path""$file_prefix"$(date +'%Y-%m-%d').log 2>&1
 # Inicialización de variables
 from_ts="";
 to_ts="";
-now=$(date +'%s');
-str_now=$(date -d @$now +'%Y-%m-%d %H:%M:%S');
+inicio=$(date +'%s');
+str_now=$(date -d @$inicio +'%Y-%m-%d %H:%M:%S');
 param_regex='^[0-9]{4}-[0-9]{2}-[0-9]{2}\ [0-9]{2}:[0-9]{2}:[0-9]{2}$';
 
-printf "\nInfo: Iniciando %s con timestamp %s (%s).\n\n" $0 "$str_now" "$now";
-
-# Importación de parametros del script
-if ! . ../conf/params.cfg; then
-    printf "Error: no se puede acceder al fichero de parametros.\n" >&2
-    exit 1;
-fi
+printf "\nInfo: Iniciando %s a las %s.\n" $0 "$str_now";
 
 # comprobamos si se puede leer el fichero de credenciales
 if [ ! -e "$mysql_loginpath" ]; then
@@ -51,69 +47,46 @@ fi
 #               $2    el formato (regex)
 # Salida:       0 si ok, 1 sino.
 function testDateFormat(){
-    if [[ $1 =~ $2 ]]; then
-        printf "ok regex\n";
-        res=date -d "$1" >/dev/null 2>&1;
-        printf "res=%s\n" "$res";
-        if [ date -d '"$1"' >/dev/null 2>&1 ]; then
-            printf "ok date\n";
+    if [[ $1 =~ $2 ]] && date --date "$1" >/dev/null 2>&1; then
             return 0;
-        fi
+    else
+        #printf "Info: fecha incorrecta: %s\n" "$1";
+        return 1;
     fi
-    return 1;
 }
 
 # primero comprobamos si se ha especificado un parametro de ejecución
-temp_from_ts="";
-if [ -n "$1" ]; then
-    # comprobamos si el argumento tiene el formato "NNNN-NN-NN NN:NN:NN"
-    printf "date:%s, regex:%s.\n" "$1" "$param_regex";
-    if testDateFormat "$1" "$param_regex"; then
-        from_ts=$1
-        printf "from_ts: %s\n" "$from_ts"
-    else
-        printf "Error: timestamp de extracción informado pero mal formateado (se espera YYYY-MM-DD HH:MM:SS).\n" >&2
-        exit 1;
-    fi
-else 
+if testDateFormat "$1" "$param_regex"; then
+    printf "Info: fecha min recuperada del primer parametro: %s.\n" "$1";
+    from_ts="$1"
+else
     # si no se ha especificado ningun argumento, la recuperamos del fichero .lastdate
-    if ! . "$lastdatefile"; then
-        printf "Error: No se ha especificado ningun argumento y el fichero %s no esta presente.\n" "$lastdatefile" >&2
-        exit 1;
+    if [ -e "$lastdatefile" ] && from_ts=$(cat "$lastdatefile") && testDateFormat "$from_ts" "$param_regex"; then
+        printf "Info: fecha min %s recuperada del fichero: %s.\n" "$from_ts" "$lastdatefile";
     else
-        from_ts=$(cat "$lastdatefile")
-        if ! testDateFormat "$from_ts" "$param_regex"; then
-            printf "Error: timestamp mal formateado en el fichero %s.\n" "$lastdatefile" >&2
-            exit 1;
-        fi
+        printf "Error: No se ha especificado ningun argumento valida y el fichero %s no contiene una fecha valida.\n" "$lastdatefile" >&2
+        exit 1;
     fi
 fi
 
 # comprobamos si se ha especificado el segundo parametro de ejecución
-if [ -n "$2" ]; then
-    # comprobamos si el argumento tiene el formato "NNNN-NN-NN NN:NN:NN"
-    printf "date2:%s, regex:%s.\n" "$2" "$param_regex";
-    if testDateFormat "$2" "$param_regex"; then
-        to_ts="$2";
-        printf "to_ts: %s\n" "$from_ts"
-    else
-        printf "Error: timestamp de maxima extracción informado pero mal formateado (se espera YYYY-MM-DD HH:MM:SS).\n" >&2
-        exit 1;
-    fi
+if testDateFormat "$2" "$param_regex"; then
+    printf "Info: fecha max recuperada del segundo parametro: %s.\n" "$2";
+    to_ts="$2";
 else
     printf "Info: no se ha especificado un segundo parametro, se usa %s.\n" "$str_now";
     to_ts=$str_now;
 fi
 
-# nombre del fichero de salida
-fichero="$file_prefix""$to_ts".tar.gz
+
 
 # lanzamos el script sql
 mysql --login-path="$mysql_loginpath" --database="$mysql_db" --execute=" \
-set @from_ts='${from_ts}'; \
-set @to_ts='${to_ts}'; \
-set @unload_dir='${unload_dir}'; \
-set @file_prefix='${file_prefix}'; \
+SET @from_ts='${from_ts}'; \
+SET @to_ts='${to_ts}'; \
+SET @unload_dir='${unload_dir}'; \
+SET @file_prefix='${file_prefix}'; \
+SET @file_datefmt='${mysql_datefmt}'; \
 source ${sql_file};"
 
 resultado=$?
@@ -125,9 +98,14 @@ else
 fi
 
 # Probamos a ver si se han creado los ficheros resultantes
-patron_ficheros="$file_prefix"\*_"$to_ts".dat
+patron_ficheros="$file_prefix"\*_$(date --date="$to_ts" +"$bash_datefmt").dat
+
+# nombre del fichero tar.gz de salida
+fichero="$file_prefix"$(date --date="$to_ts" +"$bash_datefmt").tar.gz
+
+cd "$unload_dir";
 for f in $patron_ficheros; do
-    # si no hay ficheros, el test a continuación dara "false", sino pues tenemos claro que hay por lo menos un fichero.
+    # si no hay ficheros, el test a continuación dara "false", sino tenemos claro que hay por lo menos un fichero.
     if [ -f "$f" ]; then
         files_exist=SI
     else
@@ -138,22 +116,33 @@ done
 
 # Si hay ficheros resultantes, los comprimimos en un tar.gz
 if [ "$files_exist" == "SI" ]; then
-    tar --remove-files -czvf "$fichero" "$patron_ficheros"
+    tar --remove-files -czv --file "$fichero" $patron_ficheros
     resultado=$?
     if [ "$resultado" != 0 ]; then
         printf "Error: error al generar el archivo comprimido %s\n" "$fichero"
-        exit "$resultado";
+    else 
+        printf "Info: Generado el archivo comprimido %s\n" "$fichero"
     fi
 else
     printf "Error: no se ha generado ninguna descarga.\n" >&2
-    exit 1;
 fi
-printf "Info: Generado el archivo comprimido %s\n" "$fichero"
+
+# Si todo ha ido bien, guardamos la fecha en lastdatefile para la proxima ejecución.
+if [ "$resultado" == 0 ]; then
+    echo "$to_ts" > "$lastdatefile";
+fi
+
+
+# Borramos los datos de mas de 30 días
+find "$unload_dir""$file_prefix"*.tar.gz -type f -mtime +30 -delete;
 
 # Comprimimos los logs de mas de 30 días
-find ../logs/*.log -mtime +30 -exec tar --remove-files -cvfz {}.tar.gz {};
+find "$logs_path""$file_prefix"*.log -type f -mtime +30 -execdir tar --remove-files -cvz --file {}.tar.gz {} \;
 
 # Borramos los logs de mas de 90 días
-find ../logs/*.tar.gz -mtime +90 -delete;
+find "$logs_path""$file_prefix"*.tar.gz -type f -mtime +90 -delete;
 
+fin=$(date +'%s');
+duration=$(( $fin - $inicio ));
+printf "Info: Descarga finalizada en %u min %u sec.\n" $(($duration/60)) $(($duration%60))
 exit "$resultado";
--- unload/sql/unload_gorrion.sql
+++ unload/sql/unload_gorrion.sql
@@ -1,13 +1,14 @@
 
 SET @ts_format='%Y-%m-%d_%H:%i:%S';
-SET @filets_format='%Y-%m-%d_%H.%i.%S'; -- hay un pb con los ":" en los nombres de fichero
 SET @date_format='%Y-%m-%d';
 SET @fieldseparator='¬';
 SET @endofline='\n';
 
 -- Descarga total de la tabla descripteur
-SET @output_file = CONCAT(@unload_dir, @file_prefix, "_descripteur_", DATE_FORMAT(@until_ts,@filets_format), ".dat");
-SET @query = CONCAT("SELECT `T_DESCRIPTEUR_ID`,
+SET @table = 'descripteur';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `T_DESCRIPTEUR_ID`,
   `LibelleDescripteur`,
   `SautDescripteur`,
   `Valeur`,
@@ -17,17 +18,176 @@ SET @query = CONCAT("SELECT `T_DESCRIPTEUR_ID`,
   `description`,
   `isSession`,
   `saisissable`,
-  '", @until_ts, "' as `unload_dt`
-FROM `descripteur`
+  '",@to_ts, "' as `unload_dt`
+FROM `",@table,"` 
 INTO OUTFILE '", @output_file, "'
 FIELDS TERMINATED BY '", @fieldseparator, "'
 LINES TERMINATED BY '", @endofline, "';");
-select @query;
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
+
+-- Descarga total de la tabla compte
+SET @table = 'compte';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `T_COMPTE_ID`,
+  `Login`,
+  `MotdePasse`, 
+  DATE_FORMAT(`DateConnexion`,'",@date_format,"'),
+  `HeureConnexion`,
+  `repertoire`,
+  `T_PERSONNE_ID`,
+  `langue`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
+
+-- Descarga total de la tabla compte_groupe
+SET @table = 'compte_groupe';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `id_projet`,
+  `T_COMPTE_ID`,
+  `cle`,
+  `typeCle`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
+
+-- Descarga total de la tabla lier_groupe
+SET @table = 'lier_groupe';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `id_lier_groupe`,
+  `id_user`,
+  `id_groupe`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
+
+-- Descarga total de la tabla liste_droits
+SET @table = 'liste_droits';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `id_liste_droits`,
+  `id_user`,
+  `Y_droit`,
+  `valeur_droit`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
+
+-- Descarga total de la tabla modalite
+SET @table = 'modalite';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `T_MODALITE_ID`,
+  `LibelleModalite`,
+  `TypeModalite`,
+  `CodeModalite`,
+  `RangModalite`,
+  `T_DESCRIPTEUR_ID`,
+  `visible`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
 PREPARE query FROM @query;
 EXECUTE query;
 DEALLOCATE PREPARE query;
 
 
+-- Descarga total de la tabla personne
+SET @table = 'personne';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `T_PERSONNE_ID`,
+  `Nom`,
+  `Prenom`,
+  DATE_FORMAT(`DateNaissance`,'",@date_format,"'),
+  `Telephone`,
+  `Email`,
+  `Succursale`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
 
 
+-- Descarga total de la tabla questionnaire
+SET @table = 'questionnaire';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `T_QUESTIONNAIRE_ID`,
+  `LibelleQuestionnaire`,
+  `CleModification`,
+  `CleSaisie`,
+  `CleSuppression`,
+  `CleValidation`,
+  `ClePublique`,
+  `CleVisualisation`,
+  `GID`,
+  `ValiditeReponse`,
+  `publicationReponse`,
+  `id_projet`,
+  `T_PAYS_ID`,
+  `CleExpert`,
+  `Suivi`,
+  `DescrGroupe`,
+  `DescrGroupe2`,
+  `DescrRech`,
+  `saisie`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
+
+
+-- Descarga total de la tabla session
+SET @table = 'session';
+SET @output_file = CONCAT(@unload_dir, @file_prefix, @table,"_",DATE_FORMAT(@to_ts,@file_datefmt),".dat");
+SET @query = CONCAT(
+  "SELECT `T_SESSION_ID`,
+  `titreSession`,
+  DATE_FORMAT(`dateSession`,'",@date_format,"'),
+  `T_COMPTE_ID`,
+  `T_QUESTIONNAIRE_ID`,
+  '", @to_ts, "' as `unload_dt`
+FROM `",@table,"` 
+INTO OUTFILE '", @output_file, "'
+FIELDS TERMINATED BY '", @fieldseparator, "'
+LINES TERMINATED BY '", @endofline, "';");
+PREPARE query FROM @query;
+EXECUTE query;
+DEALLOCATE PREPARE query;
 
