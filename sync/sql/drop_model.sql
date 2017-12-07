eff2f8c6715f57a97a5f0d460f3120259cee1767
Gegene79 <fabienvignat@yahoo.fr>
2017-12-07 10:37:47 +0100

    simplificación del modelo

--- sync/sql/drop_model.sql
+++ sync/sql/drop_model.sql
@@ -4,30 +4,13 @@
 -- Borra las tablas del datamart.
 --
 
-DROP TABLE `stg_descripteur`;
-DROP TABLE `stg_modalite`;
-DROP TABLE `stg_observation`;
-DROP TABLE `stg_obs_descript`;
-DROP TABLE `stg_obs_mod`;
-DROP TABLE `stg_personne`;
-DROP TABLE `stg_questionnaire`;
-DROP TABLE `stg_compte`;
-DROP TABLE `stg_groupe`;
-DROP TABLE `stg_lier_groupe`;
-DROP TABLE `stg_liste_droits`;
-DROP TABLE `stg_succursale_groupe`;
+
 DROP TABLE `ods_descripteur`;
 DROP TABLE `ods_modalite`;
 DROP TABLE `ods_observation`;
 DROP TABLE `ods_obs_descript`;
 DROP TABLE `ods_obs_mod`;
-DROP TABLE `ods_personne`;
-DROP TABLE `ods_questionnaire`;
-DROP TABLE `ods_compte`;
-DROP TABLE `ods_groupe`;
-DROP TABLE `ods_lier_groupe`;
-DROP TABLE `ods_liste_droits`;
-DROP TABLE `ods_succursale_groupe`;
+
 DROP TABLE `td_prestacion`;
 DROP TABLE `td_tipoprograma`;
 DROP TABLE `td_finprograma`;
